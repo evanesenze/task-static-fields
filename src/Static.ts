1a0628961d29e10137fd3643f3988a11f9c3cf6f
Ivan <64332115+evanesenze@users.noreply.github.com>
2022-03-28 00:44:39 +0500

    Static

--- src/Static.ts
+++ src/Static.ts
@@ -4,4 +4,12 @@
 3. Добавьте публичный статический метод `PI`, возвращающий значение `Math.PI`
 4. Добавьте публичный статический метод `ceil`, возвращающий число округленное в большую сторону
  */
-export default class TestClass {}
+export default class TestClass {
+    public static counterStatic: number = 0;
+
+    public counter: number = 0;
+
+    public static PI = () => Math.PI;
+
+    public static ceil = (n: number) => Math.ceil(n);
+}
